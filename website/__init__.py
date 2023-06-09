ee9fe1c8340e48ce987235b6044b5c9269dc35cc
etaranen <elizabeth.taranen@gmail.com>
2023-06-09 19:51:09 -0400

    Sign up page updated!

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = 'abc'
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
 
   from .views import views
   from .auth import auth
@@ -10,4 +18,14 @@ def create_app():
   app.register_blueprint(views, url_prefix= "/")
   app.register_blueprint(auth, url_prefix= "/")
 
+
+  from .models as User, Note
+
+  create_database(app)
+
   return app
+
+def create_database(app):
+  if not path.exists('website/' + DB_NAME):
+    db.create_all(app=app)
+    print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,51 @@
 #Movie and Television Tracker
 
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-  return "<p>Login</p>"
+
+  #when you but the submitted form into a variable (ie.data), it is stored as a list with tuples 
+  #ie. [('email', 'liz@gmail.com'), ('password', '1234')]
+  data= request.form
+  print(data)
+  return render_template("login.html", boolean=True)
+
 
 @auth.route("/logout")
 def logout():
   return "<p>Logout</p>"
 
-@auth.route("/sign-up")
+
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
-  return "<p>Sign Up</p>"
+
+  if request.method== 'POST':
+    email = request.form.get('email')
+    firstName = request.form.get('firstName')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    address= email.find("@")
+
+    # if email[(address + 1):] != "gmail.com" or  email[(address + 1):] != "yahoo.com" or  email[(address + 1):] != "hotmail.com":
+    #   flash("Not a valid email adress", category= 'error')
+
+    if len(email) < 4:
+      flash("Email must be greater than 3 characters.", category= 'error')
+    elif len(firstName) < 2:
+      flash("First name must be greater than 1 character.", category= 'error')
+    elif password1 != password2:
+      flash("Passwords must be the same.", category= 'error')
+    elif len(password1) < 7:
+      flash("Password must be greater than 7 characters.", category= 'error')
+    else:
+      flash("Account create!", category= 'success')
+
+
+  data= request.form
+  print(data)
+  return render_template("sign_up.html")
